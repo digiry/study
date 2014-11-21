cd41b5d01f241da984f9c178208aa26e430ed556
Kyujin Kang <digiry@gmail.com>
2014-11-21 10:22:42 +0900

    Create CNode.h

---
+++ cpp_programming/sample/CNode.h
@@ -0,0 +1,18 @@
+// CList CLASS
+// 프로그램명 : CNode.h
+// 작  성  자 : NINAEROS.AGAFE
+// 작  성  일 : 05/01/25
+
+#ifndef __CNODE_H
+#define __CONDE_H
+
+template <class Type>
+class NODE
+{
+public:
+	Type data;
+	NODE* Llink;
+	NODE* Rlink;
+};
+
+#endif __CNODE_H
