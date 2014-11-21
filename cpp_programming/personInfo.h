825b3797163850345e0b980df1e80b7f5c70d1ff
Kyujin Kang <digiry@gmail.com>
2014-11-21 09:34:34 +0900

    Create personInfo.h

---
+++ cpp_programming/personInfo.h
@@ -0,0 +1,23 @@
+/*
+ * personInfoCpp.h
+ *
+ *  Created on: 2014. 11. 16.
+ *      Author: suandkyu
+ */
+
+#ifndef PERSONINFOCPP_H_
+#define PERSONINFO_H_
+
+#include <string>
+
+using namespace std;
+
+class personInfo {
+public:
+	string name;
+	string phone;
+	string address;
+};
+
+
+#endif /* PERSONINFO_H_ */
