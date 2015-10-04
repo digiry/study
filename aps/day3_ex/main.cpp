b7b019c9ff9687e1118ee8ad9c7fc9fb869a74cd
digiry <digiry@gmail.com>
2015-10-05 05:04:58 +0900

    add day3 example

---
+++ aps/day3_ex/main.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAX(x,y) ((x>y)?x:y)
+
+char input[6] = {0,};
+int input_length = 0;
+
+int max_number = 0;
+
+void copy_number(char *src, char *dst);
+
+void init_c(char *src);
+void itoc(int number, char *dst);
+int ctoi(char *src);
+
+int get_length(char *src);
+
+int switch_number(char *src, int base, int target);
+void find_max(char *src, int base);
+
+int main() {
+    int T;
+    int N;
+    int C;
+    int testCase;
+    int result;
+
+    cin >> T;
+    for (testCase=0; testCase<T; testCase++) {
+        cin >> N >> C;
+        itoc(N, input);
+        input_length = get_length(input);
+        max_number = 0;
+
+        for (int count=0; count<C; count++) {
+            find_max(input, 6-input_length);
+            itoc(max_number, input);
+        }
+        result = ctoi(input);
+
+        cout << "#" << testCase+1 << " " << result << endl;
+    }
+    return 0;
+}
+
+void init_c(char *src) {
+    for (int index=0; index<6; index++) {
+        src[index] = 0;
+    }
+}
+
+void copy_number(char *src, char *dst) {
+    for (int index=0; index<6; index++) {
+        dst[index] = src[index];
+    }
+}
+
+void itoc(int number, char *dst) {
+    int letter;
+    int length = 5;
+
+    init_c(dst);
+    while (number!=0) {
+        letter = number%10;
+        number = number/10;
+        dst[length] = '0'+letter;
+        length--;
+    }
+}
+
+int ctoi(char *src) {
+    int result = 0;
+    int index = 0;
+
+    while (index < 6) {
+        if (src[index] != 0) {
+            result = result*10;
+            result = result+(src[index]-'0');
+        }
+        index++;
+    }
+    return result;
+}
+
+int get_length(char *src) {
+    int length = 0;
+    int index = 5;
+
+    while (src[index] != 0) {
+        length++;
+        index--;
+    }
+
+    return length;
+}
+
+void find_max(char *src, int base) {
+    if (base == 5) {
+        return;
+    }
+    for (int index=base+1; index<6; index++) {
+        int temp = switch_number(src, base, index);
+        max_number = MAX(max_number, temp);
+    }
+    find_max(src, base+1);
+}
+
+int switch_number(char *src, int base, int target) {
+    char temp[6];
+    char swap;
+    int number;
+
+    copy_number(src, temp);
+    swap = temp[base];
+    temp[base] = temp[target];
+    temp[target] = swap;
+
+    number = ctoi(temp);
+
+    return number;
+}
