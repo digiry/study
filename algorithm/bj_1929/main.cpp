1fdf14928a4c75ac9a13d874743b78d253dd78e1
digiry <digiry@gmail.com>
2016-06-16 22:41:45 +0900

    add bj 1929

---
+++ algorithm/bj_1929/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int start_num;
+int end_num;
+
+bool no_primes[1000001];
+
+int main() {
+	no_primes[0] = true;
+	no_primes[1] = true;
+
+	for (int i = 2; i*i <= 1000000; i++) {
+		if (no_primes[i] == false) {
+			for (int j = i+i; j <= 1000000; j += i) {
+				no_primes[j] = true;
+			}
+		}
+	}
+
+	cin >> start_num >> end_num;
+	for (int j = start_num; j <= end_num; j++) {
+		if (no_primes[j] == false) {
+			cout << j << endl;
+		}
+	}
+
+	return 0;
+}
