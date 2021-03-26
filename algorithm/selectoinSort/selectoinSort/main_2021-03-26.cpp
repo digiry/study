7552f3d8819326478883202c18e6b0f52141d319
강규진 <ninaeros@hotmail.co.kr>
2021-03-26 11:42:04 +0900

    Add selection sort 2021-03-26

---
+++ algorithm/selectoinSort/selectoinSort/main_2021-03-26.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int ARR[] = { 3,2,5,1,4 };
+
+void PrintArray(int* const pArr, const int length) {
+	for (int i = 0; i < length; i++) {
+		cout << pArr[i] << ' ';
+	}
+	cout << endl;
+}
+
+void Swap(int* const pArr, const int a_index, const int b_index) {
+	if (a_index == b_index) {
+		return;
+	}
+
+	pArr[a_index] = pArr[a_index] ^ pArr[b_index];
+	pArr[b_index] = pArr[a_index] ^ pArr[b_index];
+	pArr[a_index] = pArr[a_index] ^ pArr[b_index];
+}
+
+void SelectionSort(int* const pArr, const int length) {
+	for (int i = 0; i < length; i++) {
+		int min = i;
+		for (int j = i; j < length; j++) {
+			if (pArr[min] > pArr[j]) {
+				min = j;
+			}
+		}
+		Swap(pArr, min, i);
+	}
+}
+
+int main() {
+	int length = sizeof(ARR) / sizeof(ARR[0]);
+
+	PrintArray(ARR, length);
+	SelectionSort(ARR, length);
+	PrintArray(ARR, length);
+
+	return 0;
+}
