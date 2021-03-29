ae96d89263995293758dc7491a670f2b5a9d3e14
강규진 <ninaeros@hotmail.co.kr>
2021-03-29 17:59:53 +0900

    Add counting sort 2021-03-29

---
+++ algorithm/countingSort/countingSort/main_2021-03-29.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+const int LENGTH = 14;
+int ARR[LENGTH] = { 0,4,2,2,0,0,1,1,0,1,0,2,4,2 };
+
+void PrintArray(int *const pArr, const int length) {
+	for (int i = 0; i < length; i++) {
+		cout << pArr[i] << ' ';
+	}
+	cout << endl;
+}
+
+void CountingSort(int *const pArr, const int length) {
+	int answers[LENGTH] = { 0, };
+	const int COUNTS_LENGTH = 10;
+	int counts[COUNTS_LENGTH] = { 0, };
+
+	for (int i = 0; i < length; i++) {
+		counts[pArr[i]]++;
+	}
+	for (int i = 1; i < COUNTS_LENGTH; i++) {
+		counts[i] += counts[i - 1];
+	}
+
+	for (int i = length - 1; i >= 0; i--) {
+		--counts[pArr[i]];
+		answers[counts[pArr[i]]] = pArr[i];
+	}
+
+	for (int i = 0; i < length; i++) {
+		pArr[i] = answers[i];
+	}
+}
+
+int main() {
+	PrintArray(ARR, LENGTH);
+	CountingSort(ARR, LENGTH);
+	PrintArray(ARR, LENGTH);
+
+	return 0;
+}
