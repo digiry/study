095ea6f38be2022626f29fcc7e265edb7d69995a
강규진 <ninaeros@hotmail.co.kr>
2021-04-01 17:06:12 +0900

    Add permutation 2021-04-01

---
+++ algorithm/permutation/permutation/main_2021-04-01.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+const int _N = 3;
+const int _R = 2;
+int ARR[_N] = { 1,2,3 };
+int TEMP[_R] = { 0, };
+
+void PrintArray(int *const pArr, const int length) {
+	for (int i = 0; i < length; i++) {
+		cout << pArr[i] << ' ';
+	}
+	cout << endl;
+}
+
+void Swap(int *const pArr, const int a_index, const int b_index) {
+	if (a_index == b_index) {
+		return;
+	}
+
+	pArr[a_index] = pArr[a_index] ^ pArr[b_index];
+	pArr[b_index] = pArr[a_index] ^ pArr[b_index];
+	pArr[a_index] = pArr[a_index] ^ pArr[b_index];
+}
+
+void Permutation(int *const pArr, const int n, const int r) {
+	if (r == 0) {
+		PrintArray(TEMP, _R);
+		return;
+	}
+
+	for (int i = n - 1; i >= 0; i--) {
+		Swap(pArr, i, n - 1);
+		TEMP[r - 1] = pArr[n - 1];
+		Permutation(pArr, n - 1, r - 1);
+		Swap(pArr, i, n - 1);
+	}
+}
+
+void PermutationDuplicated(int *const pArr, const int n, const int r) {
+	if (r == 0) {
+		PrintArray(TEMP, _R);
+		return;
+	}
+
+	for (int i = n - 1; i >= 0; i--) {
+		Swap(pArr, i, n - 1);
+		TEMP[r - 1] = pArr[n - 1];
+		PermutationDuplicated(pArr, n, r - 1);
+		Swap(pArr, i, n - 1);
+	}
+}
+
+int main() {
+	Permutation(ARR, _N, _R);
+
+	cout << "----" << endl;
+	PermutationDuplicated(ARR, _N, _R);
+
+
+	return 0;
+}
