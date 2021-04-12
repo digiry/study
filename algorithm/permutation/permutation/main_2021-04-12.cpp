8c95674fc5b03928221485e6173499a78d485484
강규진 <ninaeros@hotmail.co.kr>
2021-04-12 17:37:12 +0900

    Add permutation 2021-04-12

---
+++ algorithm/permutation/permutation/main_2021-04-12.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+const int _N = 4;
+const int _R = 3;
+int ARR[_N] = { 4,5,6,7 };
+int TEMP[_R] = { 0, };
+
+typedef struct _User { char mName[20]; int mWeight; } User;
+
+User MEMBERS[_N] = {
+	{"abc", 60},
+	{"def", 82},
+	{"ghi", 53},
+	{"jkl", 103},
+};
+
+int ARR4MEMBERS[_N] = { 0,1,2,3 };
+
+void PrintArray(const int *const pArr, const int length) {
+	for (int i = 0; i < length; i++) { 
+		cout << pArr[i] << ' ';
+	}
+	cout << endl;
+}
+
+void PrintMembers(const int *const pArr, const User *const pMembers, const int length) {
+	for (int i = 0; i < length; i++) {
+		cout << "[" << pArr[i] << "] " << pMembers[pArr[i]].mName << " :" << setw(3) << pMembers[pArr[i]].mWeight << ' ';
+	}
+	cout << endl;
+}
+
+void Swap(int *const pArr, const int a_index, const int b_index) {
+	if (a_index == b_index) {
+		return;
+	}
+
+	pArr[a_index] = pArr[a_index] ^ pArr[b_index];
+	pArr[b_index] = pArr[a_index] ^ pArr[b_index];
+	pArr[a_index] = pArr[a_index] ^ pArr[b_index];
+}  
+
+void Permutation(int *const pArr, const int n, const int r) {
+	if (r == 0) {
+		PrintArray(TEMP, _R);
+		return;
+	}
+
+	for (int i = n - 1; i >= 0; i--) {
+		Swap(pArr, n - 1, i);
+		TEMP[r - 1] = pArr[n - 1];
+		Permutation(pArr, n - 1, r - 1);
+		Swap(pArr, n - 1, i);
+	}
+}
+
+void Permutation(int* const pArr, const User *const pMembers, const int n, const int r) {
+	if (r == 0) {
+		PrintMembers(TEMP, MEMBERS, _R);
+		return;
+	}
+
+	for (int i = n - 1; i >= 0; i--) {
+		Swap(pArr, n - 1, i);
+		TEMP[r - 1] = pArr[n - 1];
+		Permutation(pArr, pMembers, n - 1, r - 1);
+		Swap(pArr, n - 1, i);
+	}
+}
+
+void PermutationDuplicated(int *const pArr, const int n, const int r) {
+	if (r == 0) {
+		PrintArray(TEMP, _R);
+		return;
+	}
+
+	for (int i = n - 1; i >= 0; i--) {
+		Swap(pArr, n - 1, i);
+		TEMP[r - 1] = pArr[n - 1];
+		PermutationDuplicated(pArr, n, r - 1);
+		Swap(pArr, n - 1, i);
+	}
+}
+
+void PermutationDuplicated(int* const pArr, const User* const pMembers, const int n, const int r) {
+	if (r == 0) {
+		PrintMembers(TEMP, pMembers, _R);
+		return;
+	}
+
+	for (int i = n - 1; i >= 0; i--) {
+		Swap(pArr, n - 1, i);
+		TEMP[r - 1] = pArr[n - 1];
+		PermutationDuplicated(pArr, pMembers, n, r - 1);
+		Swap(pArr, n - 1, i);
+	}
+}
+
+int main() {
+	Permutation(ARR, _N, _R);
+	cout << "---" << endl;
+	PermutationDuplicated(ARR, _N, _R);
+	cout << "---" << endl;
+	Permutation(ARR4MEMBERS, MEMBERS, _N, _R);
+	cout << "---" << endl;
+	PermutationDuplicated(ARR4MEMBERS, MEMBERS, _N, _R);
+
+	return 0;
+}
