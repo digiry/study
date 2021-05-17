ea9f933d5279d3206dfd3a6852ea25791102a8f9
강규진 <ninaeros@hotmail.co.kr>
2021-05-18 03:29:43 +0900

    Add swcertpro_disk_scheduling

---
+++ algorithm/swcertpro_disk_scheduling/swcertpro_disk_scheduling/user.cpp
@@ -0,0 +1,34 @@
+const int MAX = 100000;
+
+void init(int track_size, int head) {
+	// TO DO
+}
+
+void request(int track) {
+	// TO DO
+}
+
+int fcfs() {
+	int track_no = -1;	// TO DO : Need to be changed
+
+	return track_no;
+}
+
+int sstf() {
+	int track_no = -1;	// TO DO : Need to be changed
+
+	return track_no;
+}
+
+int look() {
+	int track_no = -1;	// TO DO : Need to be changed
+
+	return track_no;
+}
+
+int clook() {
+	int track_no = -1;	// TO DO : Need to be changed
+
+	return track_no;
+}
+
